1a62680b6477c4b1f083456b45bd207aaeb4e3f9
Mustafa ÖZCAN <wustafa.ozcan@gmail.com>
2023-01-12 23:51:00 +0300

    Info about exports

--- book/src/module-info.java
+++ book/src/module-info.java
@@ -1,3 +1,4 @@
 module book {
-    exports com.book.service to order;
+    //exports com.book.service; // We can export this package for amy module who requires it
+    exports com.book.service to order; // We can export this package for spesific module
 }
