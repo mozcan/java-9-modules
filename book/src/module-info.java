cac54732ba581275663ac9d82070eb6742fdfc7b
Mustafa ÖZCAN <wustafa.ozcan@gmail.com>
2023-01-13 00:15:56 +0300

    Info about exports - requires

--- book/src/module-info.java
+++ book/src/module-info.java
@@ -1,4 +1,4 @@
 module book {
-    //exports com.book.service; // We can export this package for amy module who requires it
-    exports com.book.service to order; // We can export this package for spesific module
+     exports com.book.service; // We can export this package for amy module who requires it
+    // exports com.book.service to order; // We can export this package for spesific module
 }
--- order/src/module-info.java
+++ order/src/module-info.java
@@ -1,4 +1,5 @@
 module order {
 
-    requires book;
+    requires book; // A requires clause expresses that the required module is needed at compile and run time.
+    // requires static book;  // A requires static clause expresses a dependency that is optional at run time.
 }
